d01305252b9a0ec4ec54c53a3393cc3f207f5c00
Chris DuBois <chris.dubois@gmail.com>
2012-06-19 08:58:47 -0700

    Use average of 20 runs for Julia timings.

--- examples/timeit.jl
+++ examples/timeit.jl
@@ -1,7 +1,11 @@
 macro timeit(ex,name)
     t, i = gensym(2)
     quote
-        $t =  @elapsed $ex
-        printf("%s took %.1f ms\n", $name, $t*1000)
+        $t = 0
+        for $i=1:20
+            $t += @elapsed $ex
+        end
+        $t = $t/20
+        printf("%s took %.1f ms (avg over 20 runs)\n", $name, $t*1000)
     end
 end
