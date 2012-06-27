1d8d9c058b186abf636777a1f674e522d8329b65
Chris DuBois <chris.dubois@gmail.com>
2012-06-26 22:10:05 -0700

    Add figures for multivariate example.

--- examples/plot.r
+++ examples/plot.r
@@ -30,15 +30,35 @@ for (i in c("g","h")) {   # target densities
   }
 }
 
-# Plots for multivariate example
+# Plots for multivariate example.
+f <- function(x,y) x^2 * exp(-x*y^2 - y^2 + 2*y - 4*x)
+xs <- seq(0,3,by=.01)
+ys <- seq(-1,2,by=.01)
+truth <- matrix(0,length(xs),length(ys))
+for (i in 1:length(xs)) {
+  for (j in 1:length(ys)) {
+    truth[i,j] <- f(xs[i],ys[j])
+  }
+}
+
 samples_gibbs <- read.csv("examples/multivariate/gibbs.dat",header=FALSE)
 samples_hmc <- read.csv("examples/multivariate/hmc.dat",header=FALSE)
 samples_slice <- read.csv("examples/multivariate/slice.dat",header=FALSE)
-png("compare.png",width=1000,height=300)
+png("examples/multivariate/compare.png",width=1000,height=300)
+xlims <- c(0,3)
+ylims <- c(-1,2)
 par(mfrow=c(1,3))
-plot(samples_gibbs)
-plot(samples_hmc)
-plot(samples_slice)
-  acf(samples)
-  dev.off()
-}
+image(xs,ys,truth)
+plot(samples_gibbs[1:1000,],xlim=xlims,ylim=ylims,xlab="Gibbs",ylab="")
+plot(samples_slice[1:1000,],xlim=xlims,ylim=ylims,xlab="Slice",ylab="")
+#plot(samples_hmc[1:1000,],xlim=xlims,ylim=ylims,xlab="HMC",ylab="")
+#acf(samples_slice)
+dev.off()
+
+png("examples/multivariate/trace.png",width=500,height=500)
+par(mfrow=c(2,2))
+plot(samples_gibbs[1:1000,1],type="l",xlab="Iteration",ylab="Gibbs sampling x",ylim=xlims)
+plot(samples_slice[1:1000,1],type="l",xlab="Iteration",ylab="Slice sampling x",ylim=xlims)
+plot(samples_gibbs[1:1000,2],type="l",xlab="Iteration",ylab="Gibbs sampling y",ylim=ylims)
+plot(samples_slice[1:1000,2],type="l",xlab="Iteration",ylab="Slice sampling y",ylim=ylims)
+dev.off()
