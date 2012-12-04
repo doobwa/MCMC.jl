f4142f841c7490aaf6be7cfd2785db1fd9952044
Nick Foti <nfoti01@gmail.com>
2012-12-04 18:15:58 -0500

    Updated comment.

--- src/metropolis.jl
+++ src/metropolis.jl
@@ -1,5 +1,5 @@
 
-# Univariate case
+## Univariate case
 function metropolis_sampler(x::Float64, g::Function, sd::Float64, gx::Float64)
   x1 = x + randn()*sd
   gx1 = g(x1)
@@ -20,7 +20,7 @@ metropolis_sampler(x::Float64, g::Function) = metropolis_sampler(x,g,.5)
 # Interface using Density type
 metropolis_sampler(x::Float64, g::Density, sd::Float64, gx::Float64) = metropolis_sampler(x, Density.f, sd, gx)
 
-# Multivariate case
+## Multivariate case
 function metropolis_sampler(x::Vector{Float64}, g::Function, sd::Float64, gx::Vector{Float64})
   x1 = x + sd*randn(numel(x))
   gx1 = g(x1)
