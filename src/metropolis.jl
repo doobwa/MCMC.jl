ca073a69ad056fbb17bb028e598a49f5d974cf31
Nick Foti <nfoti01@gmail.com>
2012-12-04 18:11:00 -0500

    Multivariate metropolis sampler uses randn and is passed (transpose of) Cholesky decomposition of covariance matrix.

--- src/metropolis.jl
+++ src/metropolis.jl
@@ -22,7 +22,7 @@ metropolis_sampler(x::Float64, g::Density, sd::Float64, gx::Float64) = metropoli
 
 # Multivariate case
 function metropolis_sampler(x::Vector{Float64}, g::Function, sd::Float64, gx::Vector{Float64})
-  x1 = x + randn(numel(x))*sd
+  x1 = x + sd*randn(numel(x))
   gx1 = g(x1)
   if gx1 - gx > log(rand())
     x = x1
@@ -31,12 +31,11 @@ function metropolis_sampler(x::Vector{Float64}, g::Function, sd::Float64, gx::Ve
   return x,gx
 end
 
-function metropolis_sampler(x::Vector{Float64}, g::Function, C::Matrix{Float64}, gx::Vector{Float64})
+function metropolis_sampler(x::Vector{Float64}, g::Function, sd::Matrix{Float64}, gx::Vector{Float64})
 # If C is the desired covariance of the proposal then sd = chol(C)'
-# Should probably add some error checking inc ase C is not p.s.d.
-  #sd = chol(C)' # Probably should pass sd in directly
-  #x1 = x + sd*randn(numel(x))
-  x1 = rand(MultivariateNormal(x,C))
+# The reason for pre-multiplying is to generate a vector rather than a 
+# matrix.
+  x1 = x + sd*randn(numel(x))
   gx1 = g(x1)
   if gx1 - gx > log(rand())
     x = x1
@@ -46,12 +45,12 @@ function metropolis_sampler(x::Vector{Float64}, g::Function, C::Matrix{Float64},
 end
 
 # Add in versions with default arguments
-function metropolis_sampler(x::Vector{Float64}, g::Function, C::Matrix{Float64})
+function metropolis_sampler(x::Vector{Float64}, g::Function, sd::Matrix{Float64})
   gx = g(x)
-  metropolis_sampler(x,g,C,gx)
+  metropolis_sampler(x,g,sd,gx)
 end
 
 metropolis_sampler(x::Float64, g::Function) = metropolis_sampler(x,g,.5*eye(numel(x)))
 
 # Interface using Density type
-metropolis_sampler(x::Vector{Float64}, g::Density, C::Matrix{Float64}, gx::Vector{Float64}) = metropolis_sampler(x, Density.f, C, gx)
+metropolis_sampler(x::Vector{Float64}, g::Density, sd::Matrix{Float64}, gx::Vector{Float64}) = metropolis_sampler(x, Density.f, sd, gx)
